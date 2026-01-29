f72aa5e91eae680b718153e1d1d79f063a2cca93
Shritej <shritej.nimbhorkar@worldline.com>
2026-01-29 11:12:08 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { PROJECTS } from './project-config';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'release-doc-generator';
+
+  form: FormGroup;
+  projects = PROJECTS;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient
+  ) {
+    this.form = this.fb.group({
+      projectId: ['', Validators.required],
+      version: ['', Validators.required]
+    });
+  }
+
+  downloadDoc() {
+
+    if (this.form.invalid) {
+      alert('Please select project and version');
+      return;
+    }
+
+    const { projectId, version } = this.form.value;
+
+    const selectedProject = this.projects.find(p => p.id === projectId);
+
+    if (!selectedProject) {
+      alert('Invalid project');
+      return;
+    }
+
+    this.http.get(selectedProject.templatePath, { responseType: 'text' })
+      .subscribe(template => {
+
+        const updatedDoc = template.replace(/{{VERSION}}/g, version);
+
+        const blob = new Blob(['\ufeff', updatedDoc], {
+          type: 'application/msword'
+        });
+
+        const fileName = `${projectId}_${version}.doc`;
+
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+
+      });
+  }
 }
---
+++ src/app/project-config.ts
@@ -0,0 +1,18 @@
+export const PROJECTS = [
+    {
+      id: 'keycntrl-loader-ibo',
+      name: 'KeyCntrl Loader IBO',
+      templatePath: 'assets/templates/ibo-template.html'
+    },
+    {
+      id: 'keycntrl-loader-stni',
+      name: 'KeyCntrl Loader STNI',
+      templatePath: 'assets/templates/stni-template.html'
+    },
+    {
+      id: 'keycntrl-loader-aguidef',
+      name: 'KeyCntrl Loader AGUIDEF',
+      templatePath: 'assets/templates/aguidef-template.html'
+    }
+  ];
+  
